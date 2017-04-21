90f569abe004a732941be4b6edefd6bcb6f2439f
Anthony Piddubny <anton.p@qualisystems.com>
2017-04-21 18:32:31 +0300

    update Enable/Disable SNMP flows

--- cloudshell/networking/cisco/flows/cisco_disable_snmp_flow.py
+++ cloudshell/networking/cisco/flows/cisco_disable_snmp_flow.py
@@ -3,7 +3,6 @@
 
 from cloudshell.devices.flows.cli_action_flows import DisableSnmpFlow
 from cloudshell.networking.cisco.command_actions.enable_disable_snmp_actions import EnableDisableSnmpActions
-from cloudshell.snmp.snmp_parameters import SNMPV2Parameters
 
 
 class CiscoDisableSnmpFlow(DisableSnmpFlow):
@@ -18,11 +17,7 @@ class CiscoDisableSnmpFlow(DisableSnmpFlow):
         super(CiscoDisableSnmpFlow, self).__init__(cli_handler, logger)
         self._cli_handler = cli_handler
 
-    def execute_flow(self, snmp_parameters=None):
-
-        if not isinstance(snmp_parameters, SNMPV2Parameters):
-            self._logger.debug("Unsupported SNMP version. Disable SNMP skipped")
-
+    def _execute_flow(self, snmp_parameters=None):
         with self._cli_handler.config_mode_service() as session:
             snmp_actions = EnableDisableSnmpActions(session, self._logger)
             self._logger.debug("Start Disable SNMP")
--- cloudshell/networking/cisco/flows/cisco_enable_snmp_flow.py
+++ cloudshell/networking/cisco/flows/cisco_enable_snmp_flow.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from cloudshell.devices.flows.cli_action_flows import EnableSnmpFlow
 from cloudshell.networking.cisco.command_actions.enable_disable_snmp_actions import EnableDisableSnmpActions
-from cloudshell.snmp.snmp_parameters import SNMPV2Parameters
+
+from cloudshell.devices.flows.cli_action_flows import EnableSnmpFlow
 
 
 class CiscoEnableSnmpFlow(EnableSnmpFlow):
@@ -17,23 +17,13 @@ class CiscoEnableSnmpFlow(EnableSnmpFlow):
         super(CiscoEnableSnmpFlow, self).__init__(cli_handler, logger)
         self._cli_handler = cli_handler
 
-    def execute_flow(self, snmp_parameters):
-        if not isinstance(snmp_parameters, SNMPV2Parameters):
-            message = 'Unsupported SNMP version'
-            self._logger.error(message)
-            raise Exception(self.__class__.__name__, message)
-
-        if not snmp_parameters.snmp_community:
-            message = 'SNMP community cannot be empty'
-            self._logger.error(message)
-            raise Exception(self.__class__.__name__, message)
-
+    def _execute_flow(self, snmp_parameters):
         snmp_community = snmp_parameters.snmp_community
 
         with self._cli_handler.get_cli_service(self._cli_handler.config_mode) as session:
             snmp_actions = EnableDisableSnmpActions(session, self._logger)
-
             current_snmp_config = snmp_actions.get_current_snmp_communities(session)
+
             if snmp_community not in current_snmp_config:
                 snmp_actions.enable_snmp(snmp_community)
             else:
